7076939ae13745681a5de0265fdc51beefb51246
valeriia.zakhlieniuk <valeriia.zakhlieniuk@up42.com>
2021-12-17 16:21:41 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.5"
+    id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.32"
-    kotlin("plugin.spring") version "1.4.32"
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
 group = "com.up42"
@@ -37,5 +37,5 @@ tasks.withType<Test> {
 }
 
 ktlint {
-    version.set("0.40.0")
+    version.set("0.43.1")
 }
